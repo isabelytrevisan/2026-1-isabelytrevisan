391cf3ad48b432cc864d499803a53a03b02cf105
isabelytrevisan <isabely.ot25@aluno.ifsc.edu.br>
2026-04-12 14:44:40 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "uniformes";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if (!$conexao) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+?>
