5361180d03a7292c93cc6e4ec677199fb8e9b0b3
eloizadcarli <eloizadecarli08@gmail.com>
2026-05-11 23:28:07 -0300

    Update loginCheck.php

--- site_uniformes/loginCheck.php
+++ site_uniformes/loginCheck.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
-require_once __DIR__ . "/conexao.php";
+require_once __DIR__ . "/conexao.php"; //conexão com o bd
 
-$login = $_POST["login"];
+$login = $_POST["login"];//cria variavel login
 
 // procura o login no banco
 $sql = "SELECT * FROM cliente WHERE login = '$login'";
 $resultado = mysqli_query($conexao, $sql);
 
+//faz a busca e joga o resultado para o teste
 if (mysqli_num_rows($resultado) > 0) {
 
     $usuario = mysqli_fetch_assoc($resultado);
@@ -16,7 +17,7 @@ if (mysqli_num_rows($resultado) > 0) {
     $_SESSION['idCliente'] = $usuario['idCliente'];
     $_SESSION['tipo_acesso'] = $usuario['tipo_acesso'];
 
-    header("Location: pagina-inicial.html");
+    header("Location: pagina-inicial.php");
     exit();
 
 } else {
